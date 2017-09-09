6d8722250cf90605b9e57b182657d048da2517eb
Júlio Hoffimann <julio.hoffimann@gmail.com>
2017-09-09 12:47:34 -0700

    Update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,18 @@
 using GeoStatsDevTools
 using Base.Test
 
-# write your own tests here
-@test 1 == 2
+# list of maintainers
+maintainers = ["juliohm"]
+
+# environment settings
+istravis = "TRAVIS" ∈ keys(ENV)
+ismaintainer = "USER" ∈ keys(ENV) && ENV["USER"] ∈ maintainers
+
+# list of tests
+testfiles = [
+]
+
+# run
+for testfile in testfiles
+  include(testfile)
+end
