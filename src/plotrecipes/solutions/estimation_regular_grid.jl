addd66a3de7ca1ecbb0f7ecca22d099836c31cbf
Júlio Hoffimann <julio.hoffimann@gmail.com>
2018-04-29 15:42:18 -0700

    Update estimation solution plot recipe on regular grids

--- src/plotrecipes/solutions/estimation_regular_grid.jl
+++ src/plotrecipes/solutions/estimation_regular_grid.jl
@@ -3,8 +3,7 @@
 # Licensed under the ISC License. See LICENCE in the project root.
 # ------------------------------------------------------------------
 
-@recipe function f(solution::EstimationSolution{<:RegularGrid};
-                   variables=nothing, contour=true)
+@recipe function f(solution::EstimationSolution{<:RegularGrid}; variables=nothing)
   # grid dimension and size
   sdomain = domain(solution)
   dim = ndims(sdomain)
@@ -22,9 +21,6 @@
   # plot layout: mean and variance for each variable
   layout := (length(variables), 2)
 
-  # contour type for 2D solutions
-  contourtype = contour == true ? :contourf : :heatmap
-
   for (i,var) in enumerate(variables)
     # results in grid format
     M = reshape(solution.mean[var], sz)
@@ -34,29 +30,29 @@
       x = or[1]:sp[1]:or[1]+(sz[1]-1)*sp[1]
       @series begin
         subplot := 2i - 1
-        seriestype := :path
-        legend := false
+        seriestype --> :path
+        legend --> false
         title --> string(var, " mean")
         x, M
       end
       @series begin
         subplot := 2i
-        seriestype := :path
-        legend := false
+        seriestype --> :path
+        legend --> false
         title --> string(var, " variance")
         x, V
       end
     elseif dim == 2 # plot a heat map
       @series begin
         subplot := 2i - 1
-        seriestype := contourtype
+        seriestype --> :contourf
         seriescolor --> :bluesreds
         title --> string(var, " mean")
         flipdim(rotr90(M), 2)
       end
       @series begin
         subplot := 2i
-        seriestype := contourtype
+        seriestype --> :contourf
         seriescolor --> :bluesreds
         title --> string(var, " variance")
         flipdim(rotr90(V), 2)
@@ -64,14 +60,14 @@
     elseif dim == 3 # plot a volume
       @series begin
         subplot := 2i - 1
-        seriestype := :volume
+        seriestype --> :volume
         seriescolor --> :bluesreds
         title --> string(var, " mean")
         M
       end
       @series begin
         subplot := 2i
-        seriestype := :volume
+        seriestype --> :volume
         seriescolor --> :bluesreds
         title --> string(var, " variance")
         V
