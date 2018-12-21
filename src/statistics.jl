9bc561d412f01e44f83e737634246f26162544ca
riyadm <rmuradov@stanford.edu>
2018-12-21 02:09:23 -0800

    Implement Mean, Variance and Quantile

--- src/statistics.jl
+++ src/statistics.jl
@@ -2,12 +2,12 @@ abstract type AbstractStatistic end
 
 struct Mean{D<:AbstractDomain} <: AbstractStatistic
   domain::D
-  values::Dict{Symbol, Vector} 
+  values::Dict{Symbol,Vector} 
 end
 
 function Mean(solution::SimulationSolution)
   # output dictionary
-  values = Dict(var => mean(real) for (var, real) in solution.realizations)
+  values = Dict(variable => mean(real) for (variable, real) in solution.realizations)
     
   Mean{typeof(solution.domain)}(solution.domain, values)
 end
@@ -16,22 +16,22 @@ mean(solution::SimulationSolution) = Mean(solution)
 
 struct Variance{D<:AbstractDomain} <: AbstractStatistic
   domain::D
-  values::Dict{Symbol, Vector} 
+  values::Dict{Symbol,Vector} 
 end
 
 function Variance(solution::SimulationSolution)
   # output dictionary
-  values = Dict(var => var(real) for (var, real) in solution.realizations)
+  values = Dict(variable => var(real) for (variable, real) in solution.realizations)
     
   Variance{typeof(solution.domain)}(solution.domain, values)
 end
 
 var(solution::SimulationSolution) = Variance(solution)
 
-struct Quantile{D<:AbstractDomain, P<:Real}
+struct Quantile{D<:AbstractDomain} <: AbstractStatistic
   domain::D
-  p::P
-  values::Dict{Symbol, Vector}
+  p::Float64
+  values::Dict{Symbol,Vector}
 end
 
 function Quantile(solution::SimulationSolution, p::Real)
@@ -39,29 +39,26 @@ function Quantile(solution::SimulationSolution, p::Real)
   slices(x) = [getindex.(x, i) for i in 1:npoints(solution.domain)]
   
   # output dictionary
-  values = Dict(var => quantile.(slices(real), p) for (var, real) in solution.realizations)
+  values = Dict{Symbol,Vector}()
+  
+  # loop over solution realizations
+  for (variable, real) in solution.realizations
+    values[variable] = quantile.(slices(real), p)
+  end
     
-  Quantile{typeof(solution.domain), typeof(p)}(solution.domain, p, values)
+  Quantile{typeof(solution.domain)}(solution.domain, p, values)
 end
 
 quantile(solution::SimulationSolution, p::Real) = Quantile(solution, p)
 
-quantile(solution::SimulationSolution, ps::AbstractArray) = [quantile(solution, p) for p in ps]
-
+quantile(solution::SimulationSolution, ps::AbstractVector) = [quantile(solution, p) for p in ps]
 
 # ------------
 # IO methods
 # ------------
-
 function Base.show(io::IO, statistic::Mean)
   dim = ndims(statistic.domain)
   print(io, "$(dim)D Mean")
-end
-
-function Base.show(io::IO, ::MIME"text/plain", statistic::Mean)
-  println(io, statistic)
-  println(io, "  domain: ", statistic.domain)
-  print(  io, "  variables: ", join(keys(statistic.values), ", ", " and "))
 end
 
 function Base.show(io::IO, statistic::Variance)
@@ -69,20 +66,13 @@ function Base.show(io::IO, statistic::Variance)
   print(io, "$(dim)D Variance")
 end
 
-function Base.show(io::IO, ::MIME"text/plain", statistic::Variance)
-  println(io, statistic)
-  println(io, "  domain: ", statistic.domain)
-  print(  io, "  variables: ", join(keys(statistic.values), ", ", " and "))
-end
-
 function Base.show(io::IO, statistic::Quantile)
   dim = ndims(statistic.domain)
   print(io, "$(dim)D Quantile")
 end
 
-function Base.show(io::IO, ::MIME"text/plain", statistic::Quantile)
+function Base.show(io::IO, ::MIME"text/plain", statistic::AbstractStatistic)
   println(io, statistic)
   println(io, "  domain: ", statistic.domain)
-  println(io, "  variables: ", join(keys(statistic.values), ", ", " and "))
-  print(  io, "  probability: ", statistic.p)
+  print(  io, "  variables: ", join(keys(statistic.values), ", ", " and "))
 end
