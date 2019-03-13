cc661dc029ed8c4de445d616ab02df62c7526e5f
Júlio Hoffimann <julio.hoffimann@gmail.com>
2019-03-13 14:36:05 -0300

    Update tests for partition methods

--- test/partitions.jl
+++ test/partitions.jl
@@ -1,16 +1,17 @@
 @testset "Partitions" begin
+  setify(lists) = Set(Set.(lists))
   @testset "UniformPartitioner" begin
     grid = RegularGrid{Float64}(3,3)
 
     Random.seed!(123)
     p = partition(grid, UniformPartitioner(3, false))
-    @test subsets(p) == [[1,2,3], [4,5,6], [7,8,9]]
+    @test setify(subsets(p)) == setify([[1,2,3], [4,5,6], [7,8,9]])
     p = partition(grid, UniformPartitioner(3))
-    @test subsets(p) == [[8,6,9], [4,1,7], [2,3,5]]
+    @test setify(subsets(p)) == setify([[8,6,9], [4,1,7], [2,3,5]])
 
     grid = RegularGrid{Float64}(2,3)
     p = partition(grid, UniformPartitioner(3, false))
-    @test subsets(p) == [[1,2], [3,4], [5,6]]
+    @test setify(subsets(p)) == setify([[1,2], [3,4], [5,6]])
   end
 
   @testset "DirectionPartitioner" begin
@@ -18,22 +19,22 @@
 
     # basic checks on small regular grid data
     p = partition(grid, DirectionPartitioner((1.,0.)))
-    @test subsets(p) == [[1,2,3], [4,5,6], [7,8,9]]
+    @test setify(subsets(p)) == setify([[1,2,3], [4,5,6], [7,8,9]])
 
     p = partition(grid, DirectionPartitioner((0.,1.)))
-    @test subsets(p) == [[1,4,7], [2,5,8], [3,6,9]]
+    @test setify(subsets(p)) == setify([[1,4,7], [2,5,8], [3,6,9]])
 
     p = partition(grid, DirectionPartitioner((1.,1.)))
-    @test subsets(p) == [[1,5,9], [2,6], [3], [4,8], [7]]
+    @test setify(subsets(p)) == setify([[1,5,9], [2,6], [3], [4,8], [7]])
 
     p = partition(grid, DirectionPartitioner((1.,-1.)))
-    @test subsets(p) == [[1], [2,4], [3,5,7], [6,8], [9]]
+    @test setify(subsets(p)) == setify([[1], [2,4], [3,5,7], [6,8], [9]])
 
     # opposite directions produce same partition
     dir1 = (rand(), rand()); dir2 = .-dir1
     p1 = partition(grid, DirectionPartitioner(dir1))
     p2 = partition(grid, DirectionPartitioner(dir2))
-    @test subsets(p1) == subsets(p2)
+    @test setify(subsets(p1)) == setify(subsets(p2))
 
     # partition of arbitrarily large regular grid always
     # returns the "lines" and "columns" of the grid
@@ -41,12 +42,12 @@
       grid = RegularGrid{Float64}(n,n)
 
       p = partition(grid, DirectionPartitioner((1.,0.)))
-      @test subsets(p) == [collect((i-1)*n+1:i*n) for i in 1:n]
+      @test setify(subsets(p)) == setify([collect((i-1)*n+1:i*n) for i in 1:n])
       ns = [GeoStatsDevTools.npoints(d) for d in p]
       @test all(ns .== n)
 
       p = partition(grid, DirectionPartitioner((0.,1.)))
-      @test subsets(p) == [collect(i:n:n*n) for i in 1:n]
+      @test setify(subsets(p)) == setify([collect(i:n:n*n) for i in 1:n])
       ns = [GeoStatsDevTools.npoints(d) for d in p]
       @test all(ns .== n)
     end
