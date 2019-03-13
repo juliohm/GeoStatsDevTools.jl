5db673e59c4467d7ebe4dfc97c88eceec9115de6
Júlio Hoffimann <julio.hoffimann@gmail.com>
2019-03-13 14:26:22 -0300

    Remove options atol and btol in favor or tol in DirectionPartitioner

--- src/partitions/direction_partitioner.jl
+++ src/partitions/direction_partitioner.jl
@@ -2,36 +2,22 @@
 # Licensed under the ISC License. See LICENCE in the project root.
 # ------------------------------------------------------------------
 
-@doc raw"""
-    DirectionPartitioner(direction; atol=20., btol=.95)
+"""
+    DirectionPartitioner(direction; tol=1e-6)
 
 A method for partitioning spatial objects along a given `direction`
-with angle tolerance `atol` in degrees and bandwidth tolerance `btol`.
-```
-      ________________
-     /        | btol
-    /         |             ILLUSTRATION OF DIRECTION TOLERANCES
-    ----------------->
-    \ ) atol
-     \________________
-```
+with bandwidth tolerance `tol`.
 """
 struct DirectionPartitioner{T<:Real,N} <: AbstractSpatialFunctionPartitioner
   direction::SVector{N,T}
-  atol::Float64
-  btol::Float64
+  tol::Float64
 end
 
-DirectionPartitioner(direction::NTuple{N,T}; atol=20., btol=.95) where {T<:Real,N} =
-  DirectionPartitioner{T,N}(normalize(SVector(direction)), deg2rad(atol), btol)
+DirectionPartitioner(direction::NTuple{N,T}; tol=1e-6) where {T<:Real,N} =
+  DirectionPartitioner{T,N}(normalize(SVector(direction)), tol)
 
 (p::DirectionPartitioner)(x, y) = begin
+  d = p.direction
   @. y = x - y
-  l = norm(y)
-  @. y = y / l
-
-  θ = acos(p.direction ⋅ y)
-  θ > π/2 && (θ = π - θ)
-
-  θ < p.atol && l*sin(θ) < p.btol
+  norm(y .- (y ⋅ d) .* d) < p.tol
 end
