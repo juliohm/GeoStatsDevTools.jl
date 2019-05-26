034e09fca684a8ee5080f759b2227cfa37c88699
Júlio Hoffimann <julio.hoffimann@gmail.com>
2019-05-25 21:45:16 -0300

    Refactor NearestNeighborSearcher

--- src/neighborsearch/nearest.jl
+++ src/neighborsearch/nearest.jl
@@ -3,39 +3,33 @@
 # ------------------------------------------------------------------
 
 """
-    NearestNeighborSearcher(domain, K, [metric])
+    NearestNeighborSearcher(domain, locations, K, metric)
 
-A search method that finds at most `K` nearest neighbors in `domain`
-according to `metric` (default to `Euclidean()`).
+A search method that finds `K` nearest neighbors in `domain`
+`locations` according to `metric`.
 """
 struct NearestNeighborSearcher{KD<:KDTree} <: AbstractNeighborSearcher
   kdtree::KD
   K::Int
-  locations::Vector{Int}
+  locs::Vector{Int}
 end
 
-function NearestNeighborSearcher(domain::AbstractDomain, K::Int,
-                                 metric::Metric, locations::AbstractVector{Int})
-  @assert 1 ≤ K ≤ length(locations) "number of neighbors must be smaller than number of data locations"
-  @assert length(locations) ≤ npoints(domain) "number of data locations must be smaller than number of points"
-  kdtree = KDTree(coordinates(domain, locations), metric)
-  NearestNeighborSearcher{typeof(kdtree)}(kdtree, K, locations)
+function NearestNeighborSearcher(domain::AbstractDomain, locs::AbstractVector{Int}, K::Int, metric::Metric)
+  @assert 1 ≤ K ≤ length(locs) "number of neighbors must be smaller than number of data locations"
+  @assert length(locs) ≤ npoints(domain) "number of data locations must be smaller than number of points"
+  kdtree = KDTree(coordinates(domain, locs), metric)
+  NearestNeighborSearcher{typeof(kdtree)}(kdtree, K, locs)
 end
 
-function search!(neighbors::AbstractVector{Int},
-                 xₒ::AbstractVector{T},
-                 searcher::NearestNeighborSearcher,
-                 mask::AbstractVector{Bool}) where {T<:Real,N}
-  inds, _ = knn(searcher.kdtree, xₒ, searcher.K, true)
-  path = view(searcher.locations, inds)
+function search!(neighbors::AbstractVector{Int}, xₒ::AbstractVector{T},
+                 searcher::NearestNeighborSearcher, mask::AbstractVector{Bool}) where {T<:Real,N}
+  K       = searcher.K
+  inds, _ = knn(searcher.kdtree, xₒ, K, true)
+  locs    = view(searcher.locs, inds)
 
-  nneigh = 0
-  @inbounds for loc in path
-    if mask[loc]
-      nneigh += 1
-      neighbors[nneigh] = loc
-    end
+  @inbounds for i in 1:K
+    neighbors[i] = locs[i]
   end
 
-  nneigh
+  K
 end
