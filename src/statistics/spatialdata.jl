7085f75a87a0517cf25bdaeb85c5d88c0c33d309
Júlio Hoffimann <julio.hoffimann@gmail.com>
2019-06-05 19:12:49 -0300

    Fix quantile methods for spatial data

--- src/statistics/spatialdata.jl
+++ src/statistics/spatialdata.jl
@@ -14,7 +14,7 @@ var(d::WeightedSpatialData) = Dict(v => var(d, v) for (v,V) in variables(d))
 
 quantile(d::WeightedSpatialData, v::Symbol, p) = quantile(values(d, v), weights(d.weights), p)
 quantile(d::WeightedSpatialData, p::Real) = Dict(v => quantile(d, v, p) for (v,V) in variables(d))
-quantile(d::WeightedSpatialData, ps::AbstractVector{Real}) = Dict(v => quantile(d, v, ps) for (v,V) in variables(d))
+quantile(d::WeightedSpatialData, ps::AbstractVector) = Dict(v => quantile(d, v, ps) for (v,V) in variables(d))
 
 #---------------
 # SPATIAL DATA
@@ -28,17 +28,18 @@ var(d::AbstractSpatialData, blockside::Real) = var(d, BlockWeighter(blockside))
 var(d::AbstractSpatialData) = var(d, median_distance(d))
 
 quantile(d::AbstractSpatialData, p::Real, w::AbstractWeighter) = quantile(weight(d, w), p)
-quantile(d::AbstractSpatialData, ps::AbstractVector{Real}, w::AbstractWeighter) = quantile(weight(d, w), ps)
+quantile(d::AbstractSpatialData, ps::AbstractVector, w::AbstractWeighter) = quantile(weight(d, w), ps)
 quantile(d::AbstractSpatialData, p::Real, blockside::Real) = quantile(weight(d, BlockWeighter(blockside)), p)
-quantile(d::AbstractSpatialData, ps::AbstractVector{Real}, blockside::Real) = quantile(weight(d, BlockWeighter(blockside)), ps)
+quantile(d::AbstractSpatialData, ps::AbstractVector, blockside::Real) = quantile(weight(d, BlockWeighter(blockside)), ps)
 quantile(d::AbstractSpatialData, p::Real) = quantile(d, p, median_distance(d))
-quantile(d::AbstractSpatialData, ps::AbstractVector{Real}) = quantile(d, ps, median_distance(d))
+quantile(d::AbstractSpatialData, ps::AbstractVector) = quantile(d, ps, median_distance(d))
 
 function median_distance(d::AbstractSpatialData)
+  # select at most 100 points at random
   N = npoints(d)
-  n = min(N, 100)
-  inds = unique(rand(1:N, n))
+  inds = unique(rand(1:N, min(N, 100)))
   X = coordinates(d, inds)
   D = pairwise(Euclidean(), X, dims=2)
+
   median(D)
 end
