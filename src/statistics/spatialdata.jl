097416bff92a6f24a7bc453781c8d1302cff7391
Júlio Hoffimann <julio.hoffimann@gmail.com>
2019-06-06 08:20:53 -0300

    Add more methods for statistics with spatial data

--- src/statistics/spatialdata.jl
+++ src/statistics/spatialdata.jl
@@ -2,6 +2,8 @@
 # Licensed under the ISC License. See LICENCE in the project root.
 # ------------------------------------------------------------------
 
+const RealOrVec = Union{Real,AbstractVector}
+
 #------------------------
 # WEIGHTED SPATIAL DATA
 #------------------------
@@ -12,27 +14,32 @@ var(d::WeightedSpatialData, v::Symbol) = var(values(d, v), weights(d.weights),
                                              mean=mean(d, v), corrected=false)
 var(d::WeightedSpatialData) = Dict(v => var(d, v) for (v,V) in variables(d))
 
-quantile(d::WeightedSpatialData, v::Symbol, p) = quantile(values(d, v), weights(d.weights), p)
-quantile(d::WeightedSpatialData, p::Real) = Dict(v => quantile(d, v, p) for (v,V) in variables(d))
-quantile(d::WeightedSpatialData, ps::AbstractVector) = Dict(v => quantile(d, v, ps) for (v,V) in variables(d))
+quantile(d::WeightedSpatialData, v::Symbol, p::T) where {T<:RealOrVec} = quantile(values(d, v), weights(d.weights), p)
+quantile(d::WeightedSpatialData, p::T) where {T<:RealOrVec} = Dict(v => quantile(d, v, p) for (v,V) in variables(d))
 
 #---------------
 # SPATIAL DATA
 #---------------
+mean(d::AbstractSpatialData, v::Symbol, w::AbstractWeighter) = mean(weight(d, w), v)
+mean(d::AbstractSpatialData, v::Symbol, blockside::Real) = mean(d, v, BlockWeighter(blockside))
+mean(d::AbstractSpatialData, v::Symbol) = mean(d, v, median_distance(d))
 mean(d::AbstractSpatialData, w::AbstractWeighter) = mean(weight(d, w))
 mean(d::AbstractSpatialData, blockside::Real) = mean(d, BlockWeighter(blockside))
 mean(d::AbstractSpatialData) = mean(d, median_distance(d))
 
+var(d::AbstractSpatialData, v::Symbol, w::AbstractWeighter) = var(weight(d, w), v)
+var(d::AbstractSpatialData, v::Symbol, blockside::Real) = var(d, v, BlockWeighter(blockside))
+var(d::AbstractSpatialData, v::Symbol) = var(d, v, median_distance(d))
 var(d::AbstractSpatialData, w::AbstractWeighter) = var(weight(d, w))
 var(d::AbstractSpatialData, blockside::Real) = var(d, BlockWeighter(blockside))
 var(d::AbstractSpatialData) = var(d, median_distance(d))
 
-quantile(d::AbstractSpatialData, p::Real, w::AbstractWeighter) = quantile(weight(d, w), p)
-quantile(d::AbstractSpatialData, ps::AbstractVector, w::AbstractWeighter) = quantile(weight(d, w), ps)
-quantile(d::AbstractSpatialData, p::Real, blockside::Real) = quantile(weight(d, BlockWeighter(blockside)), p)
-quantile(d::AbstractSpatialData, ps::AbstractVector, blockside::Real) = quantile(weight(d, BlockWeighter(blockside)), ps)
-quantile(d::AbstractSpatialData, p::Real) = quantile(d, p, median_distance(d))
-quantile(d::AbstractSpatialData, ps::AbstractVector) = quantile(d, ps, median_distance(d))
+quantile(d::AbstractSpatialData, v::Symbol, p::T, w::AbstractWeighter) where {T<:RealOrVec} = quantile(weight(d, w), v, p)
+quantile(d::AbstractSpatialData, v::Symbol, p::T, blockside::Real) where {T<:RealOrVec} = quantile(d, v, p, BlockWeighter(blockside))
+quantile(d::AbstractSpatialData, v::Symbol, p::T) where {T<:RealOrVec} = quantile(d, v, p, median_distance(d))
+quantile(d::AbstractSpatialData, p::T, w::AbstractWeighter) where {T<:RealOrVec} = quantile(weight(d, w), p)
+quantile(d::AbstractSpatialData, p::T, blockside::Real) where {T<:RealOrVec} = quantile(d, p, BlockWeighter(blockside))
+quantile(d::AbstractSpatialData, p::T) where {T<:RealOrVec} = quantile(d, p, median_distance(d))
 
 function median_distance(d::AbstractSpatialData)
   # select at most 100 points at random
