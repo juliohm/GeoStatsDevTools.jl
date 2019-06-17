0e5411f23f5507a1212b62796894d77122dc6770
Júlio Hoffimann <julio.hoffimann@gmail.com>
2019-06-17 16:01:59 -0300

    Move types to GeoStatsBase.jl

--- src/GeoStatsDevTools.jl
+++ src/GeoStatsDevTools.jl
@@ -5,49 +5,7 @@
 module GeoStatsDevTools
 
 using Reexport
-using Random
-using StatsBase
-using Distances
-using LinearAlgebra
-using StaticArrays
-using NearestNeighbors
-using RecipesBase
-using CSV
 
-@reexport using Distributions
-
-using GeoStatsBase
-
-# implement methods for spatial objects
-import GeoStatsBase: domain, variables, value
-
-# implement methods for spatial statistics
-import Statistics: mean, var, quantile
-
-# neighborhood search
-include("neighborsearch.jl")
-
-# utilities
-include("utils.jl")
-
-# spatial statistics
-include("statistics.jl")
-
-# plot recipes
-include("plotrecipes/partitions.jl")
-include("plotrecipes/weighting.jl")
-
-export
-  # neighborhood search
-  NearestNeighborSearcher,
-  LocalNeighborSearcher,
-  search!,
-
-  # utilities
-  readgeotable,
-
-  # statistics
-  mean, var,
-  quantile
+@reexport using GeoStatsBase
 
 end
